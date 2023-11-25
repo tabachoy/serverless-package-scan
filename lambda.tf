71250f678be64058d6f3bf77be54396a2cdeb010
Emmanuel Rebellon <emman.rebellon@gmail.com>
2023-11-25 12:12:09 +0800

    push 4 rename to mjs and update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -2,7 +2,7 @@
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "./files/${var.lambda_file_name}.js"
+  source_file = "./files/${var.lambda_file_name}.mjs"
   output_path = "./files/${var.lambda_file_name}.zip"
 }
 
@@ -10,7 +10,8 @@ data "archive_file" "lambda" {
 
 resource "aws_lambda_function" "own_lambda" {
   function_name    = var.lambda_function_name
-  handler          = "lambda_function.lambda_handler"
+  #handler          = "lambda_function.lambda_handler"
+  handler          = "index.handler"
   runtime          = "nodejs14.x"
   role              = aws_iam_role.iam_for_lambda.arn
   filename         = data.archive_file.lambda.output_path                      # Adjust the path
